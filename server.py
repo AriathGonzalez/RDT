4a86b1158a2ea7a2f9f5b3a8b128ec60affeb91e
AriathGonzalez <84883360+AriathGonzalez@users.noreply.github.com>
2024-03-15 17:19:29 -0600

    Update server.py

--- server.py
+++ server.py
@@ -30,65 +30,89 @@ MSS = 1000
 
 
 def create_checksum(i, data):
-    '''
-    Get sum of bits -> 1's Complement (Flip bits) = checksum
-    '''
-    # seq_bits = bin(i)[2:].zfill(4)  # test
+    """
+    Creates a checksum for the given data.
 
-    # data = seq_bits + data  # test
+    Parameters:
+        i (int): Sequence number.
+        data (bytes): Data for which checksum is to be created.
 
-    dataSum = len(data)
-    bitSum = bin(dataSum)[2:]
+    Returns:
+        str: Computed checksum.
+    """
+    data_sum = len(data)
+    bit_sum = bin(data_sum)[2:]
 
     # Pad to 10 bits
-    while len(bitSum) < 10: # 10 before
-        bitSum = '0' + bitSum
+    while len(bit_sum) < 10: 
+        bit_sum = '0' + bit_sum
 
     checksum = ''
-    for bit in bitSum:
+    for bit in bit_sum:
         checksum += '1' if bit == '0' else '0'
 
     return checksum
 
 # TODO: Add the seq to the checksum, that's what
 def verify_checksum(i, checksum, data):
-    print("server-verify-checksum: ", checksum)
-    #print("checksum[:5]: ", checksum[:5])
-    #print("checksum[-1]: ", int(checksum[5:]))
-    # print("i: ", i)
-    # if checksum[:5] == b'FIRST' and int(checksum[5:]) == i:
-    #     return True
-    # if checksum[:4] == b'DONE' and int(checksum[4:]) == i:
-    #     return True
-    
-    # received_seq = int(checksum[:4], 2)
-    # seq_bits = bin(i)[2:].zfill(4)  # test
+    """
+    Verifies the checksum of the received data.
 
-    # seqSum = bin(received_seq + int(seq_bits, 2))
-    # for bit in seqSum:
-    #     if bit == '0':
-    #         return False
-    
-    # logic to verify checksum
-    # Add the sum and the checksum, if any 0 in found, corruption found
-    if (checksum == b'DONE' or checksum == b'FIRST'): 
-        print("Received done, time to finish")
+    Parameters:
+        i (int): Sequence number.
+        checksum (str): Received checksum.
+        data (bytes): Received data.
+
+    Returns:
+        bool: True if the checksum is valid, False otherwise.
+    """
+    if checksum == b'DONE' or checksum == b'FIRST':
         return True
-    # if i + seq in checksum
 
-    # received_seq = int(checksum[:4], 2)
-    # if received_seq != i:
-    #     return False
-    
-    dataSum = len(data)
-    totalSum = dataSum + int(checksum, 2)
-    totalSumBits = bin(totalSum)[2:]
-    for bit in totalSumBits:
+    data_sum = len(data)
+    total_sum = data_sum + int(checksum, 2)
+    total_sum_bits = bin(total_sum)[2:]
+    for bit in total_sum_bits:
         if bit == '0':
             return False
     return True
 
 def main():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Bind the socket to the port
+    server_address = (HOST, port)
+    sock.bind(server_address)
+
+    expected_seq = 0
+    while True:
+        pkt, addr = udt.recv(sock)
+        seq, checksum, data_rcvd = packet.extract(pkt)
+
+        if verify_checksum(seq, checksum, data_rcvd):
+            # TODO: Right, Client doens't care about ack for last pkt or DONE
+            if checksum == b'DONE':
+                sock.close()
+                break
+
+            if checksum == b'FIRST':
+                extension = data_rcvd.decode("utf-8")
+                expected_seq = 1
+
+            if checksum != b"FIRST" and (expected_seq % 2) == (seq % 2): # and client received ack and within client timeout
+                expected_seq += 1
+                output_file = f"{CACHE}{addr[0]}_{addr[1]}.{extension}"
+                with open(output_file, 'ab') as file:
+                    file.write(data_rcvd)
+
+            ack_to_send = "ACK - " + str(seq % 2)
+            ackpkt = packet.make(expected_seq % 2, create_checksum(expected_seq % 2 , ack_to_send).encode('utf-8'), bytes(ack_to_send, 'utf-8'))   # TODO: all seq here, were originally i
+            udt.send(ackpkt, sock, addr)
+            print("Server: Ack sent - %s", ack_to_send)
+            
+
+if __name__ == "__main__":
     # Ask for a port number and the protocol to use
     if len(sys.argv) < 5:
         print('Usage : "python server.py -p port -r protocol (0 - SnW, 1 - GBN)"\n[port: port number that server will bind and listen on]\n[protocol: protocol the server will use (SnW or GBN)]')
@@ -106,41 +130,5 @@ def main():
     # Variables we need
     port = int(args['-p'])
     protocol = int(args['-r'])
-
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    # Bind the socket to the port
-    server_address = (HOST, port)
-    sock.bind(server_address)
-    seq = 0
-    while True:
-        pkt, addr = udt.recv(sock)
-        print("server-pkt: ", pkt)
-        print("server-addr: ", addr)
-        seq, checksum, dataRcvd = packet.extract(pkt)
-        print("server-seq rcvd: ", seq)
-        print("server-checksum rcvd: ", checksum)
-        print("Server: Pkt Received - ", dataRcvd)
-        if verify_checksum(seq, checksum, dataRcvd):
-            if checksum == b'DONE':
-                print("in dataRcvd == DONE")
-                sock.close()
-                break
-            if checksum == b'FIRST':
-                extension = dataRcvd.decode("utf-8")
-            if checksum != b"FIRST":
-                # TODO: Save dataRcvd to servercache
-                outputFile = f"{CACHE}{addr[0]}_{addr[1]}.{extension}"
-                with open(outputFile, 'ab') as file:
-                    file.write(dataRcvd)
-
-            ackToSend = "ACK - " + str(seq % 2)
-            print("acktosend: ", ackToSend)
-            ackpkt = packet.make(seq % 2, create_checksum(seq % 2 , ackToSend).encode('utf-8'), bytes(ackToSend, 'utf-8'))   # TODO: all seq here, were originally i
-            udt.send(ackpkt, sock, addr)
-            print("Server: Ack sent - %s", ackToSend)
-            
-
-if __name__ == "__main__":
+    
     main()
