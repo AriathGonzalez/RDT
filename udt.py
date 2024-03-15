af2c0d90bddbad45c9eb1de8a1fee7657273bf39
AriathGonzalez <84883360+AriathGonzalez@users.noreply.github.com>
2024-03-15 17:20:27 -0600

    Update udt.py

--- udt.py
+++ udt.py
@@ -10,17 +10,12 @@ CORR_PROB = .5
 # Packet may be lost or corrupted
 def send(packet, sock, addr):
     if random.randint(0, 10) > DROP_PROB:
-        print("UDT: sending pkt: ", packet)
         sock.sendto(packet, addr)
-    return
 
 # Receive a packet from the unreliable channel
 def recv(sock):
     try:
         packet, addr = sock.recvfrom(1024)
-        print("UDT-pkt: ", packet)
-        print("UDT-addr: ", addr)
         return packet, addr
     except socket.error:
         return None, None
-
